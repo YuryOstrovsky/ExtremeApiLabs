d0ce1e90dead85ae22268d006eb12cbffe0e7387
YuryOstrovsky <yostrovs@extremenetworks.com>
2025-10-03 22:16:20 +0000

    Local updates

--- start_lab.sh
+++ start_lab.sh
@@ -2,5 +2,5 @@
 cd /home/jupyter
 /usr/bin/echo "8888" >/home/jupyter/ports.txt
 #/usr/bin/python3 app.py
-/home/jupyter/.local/bin/gunicorn --limit-request-line 8190  --bind 0.0.0.0:5000 --certfile=/home/jupyter/cert.pem --keyfile=/home/jupyter/key.pem wsgi:app
+/home/jupyter/.local/bin/gunicorn -c /home/jupyter/gunicorn.conf.py --limit-request-line 8190  --bind 0.0.0.0:5000 --certfile=/home/jupyter/cert.pem --keyfile=/home/jupyter/key.pem wsgi:app
 
